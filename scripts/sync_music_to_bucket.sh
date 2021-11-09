c19d588bfcd11ae2f7bf11f535f67e9d34fc5c35
Victor Pongnian <victor.pongnian@gmail.com>
2021-11-10 00:27:28 +0100

    state after rasp restart

--- scripts/sync_music_to_bucket.sh
+++ scripts/sync_music_to_bucket.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rclone -vv sync /Volumes/SG-VICTOR/LibraryVictor/ \
+rclone -vvv sync /Volumes/SG-VICTOR/LibraryVictor/ \
     --ignore-existing \
     --exclude Compilation/ \
     --exclude .DS_Store \
+    --no-update-modtime \
     media-server-data-folder:media/music
     #scaleway:media-bucket/music
